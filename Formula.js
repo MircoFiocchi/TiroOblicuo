ea366a3b9026324f3435e6ba336acb604e5816ce
Mirco <mircofiocchicat>
2016-06-16 10:19:08 -0300

    Cambiado2

--- Formula.js
+++ Formula.js
@@ -1,14 +1,12 @@
 function cargarDistancia(){
-    
-    var el=document.getElementById("ingresardatos").elements;
 
+    var el=document.getElementById("ingresardatos").elements;
     var url = 'listo.html#' + "#" + el["Vo"].value + "#" + el["titao"].value;
     window.location.replace(url);
 
 }
 
 function cargarTiempo(){
-
     var el=document.getElementById("ingresardatos").elements;
 
     var url = 'listo.html#' + "#" + el["Vo"].value ;
@@ -27,13 +25,16 @@ function cargarAltura(){
 function MostrarDist() {
 
     var myArr = window.location.hash.split('#');
-    var Vo = (myArr[1]);
-    var titao = (myArr[2]);
+    var Vo = (myArr[2]);
+    var titao = (myArr[3]);
+    var titaoRad;
 
-    var dist = (((document.getElementById("Vo").value ^ 2) * (Math.sin(document.getElementById("titao")))) / 9.8);
+    titaoRad=titao*Math.PI/180;
+
+    var dist = ((Vo* Math.sin(titao)) / 9.8);
     var unidad = "m";
 
-    document.getElementById('rdist').value = dist + unidad;
+    document.getElementById('rDist').value = dist+unidad;
 
 }
 
